130e1b703ea1e977a58730497481ee11c05dd847
Daviruss1969 <lukasloiodic@gmail.com>
2025-01-16 15:53:13 +0100

    [FEAT] Add current results

---
+++ s334726.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+# Example
+def f0(x: np.ndarray) -> np.ndarray:
+    return x[0] + np.sin(x[1]) / 5
+
+def f1(x: np.ndarray) -> np.ndarray:
+    return np.sin(x[0])
+
+def f2(x: np.ndarray) -> np.ndarray:
+    raise Exception("not implemented")
+
+def f3(x: np.ndarray) -> np.ndarray:
+    return np.subtract(np.logaddexp(np.add(np.square(x[0]), np.subtract(np.logaddexp2(np.logaddexp2(np.remainder(np.tanh(np.radians(x[1])), np.absolute(np.remainder(np.sinh(x[0]), np.absolute(-28.7677)))), np.remainder(x[2], x[2])), np.remainder(np.tanh(np.rint(x[1])), np.logaddexp(np.fabs(np.multiply(np.logaddexp(x[1], x[1]), np.remainder(np.positive(x[1]), np.absolute(-28.7677)))), x[2]))), np.positive(x[2]))), np.sinh(x[0])), np.add(np.subtract(np.expm1(x[1]), x[1]), x[2]))
+
+def f4(x: np.ndarray) -> np.ndarray:
+    return np.fmin(np.logaddexp2(np.multiply(np.cos(np.multiply(np.exp(np.log1p(np.cos(x[1]))), np.cbrt(np.degrees(np.cos(np.sign(x[1])))))), np.cbrt(np.degrees(-2.56006))), np.logaddexp2(np.logaddexp2(np.multiply(np.cos(np.exp2(7.40499)), np.cbrt(np.degrees(np.cos(x[1])))), np.multiply(np.multiply(np.cos(np.exp2(7.40499)), np.cbrt(np.degrees(np.cos(x[1])))), np.exp(np.cos(np.cos(np.sign(x[1])))))), np.multiply(np.multiply(np.cos(np.exp2(7.40499)), np.cbrt(np.degrees(np.cos(x[1])))), np.exp(np.cos(np.cos(-2.56006)))))), np.logaddexp2(np.multiply(np.cos(x[1]), np.cbrt(np.degrees(7.40499))), np.maximum(np.multiply(np.cos(np.exp2(7.40499)), np.cbrt(1.93907)), np.multiply(np.multiply(np.cos(np.exp2(7.40499)), np.cbrt(np.degrees(np.cos(x[1])))), np.exp(np.cos(np.cos(np.sign(2.41695))))))))
+
+def f5(x: np.ndarray) -> np.ndarray:
+    return np.remainder(np.remainder(np.remainder(np.spacing(x[0]), np.radians(2.35549e-08)), np.radians(np.arcsinh(np.negative(2.35549e-08)))), np.negative(np.expm1(np.remainder(np.spacing(x[0]), 2.35549e-08))))
+
+def f6(x: np.ndarray) -> np.ndarray:
+    return np.multiply(np.add(np.add(np.add(np.minimum(x[1], np.floor(np.exp(-10.4012))), np.add(np.minimum(np.negative(x[0]), np.expm1(np.fmod(x[1], x[1]))), x[1])), np.square(np.tanh(np.add(x[1], np.square(np.tanh(x[1])))))), np.square(np.tanh(x[0]))), 0.977212)
+
+def f7(x: np.ndarray) -> np.ndarray:
+    return np.logaddexp(np.subtract(np.add(np.exp2(-426.411), np.cosh(np.expm1(np.arcsinh(np.multiply(x[0], x[1]))))), np.fmin(np.multiply(np.fmax(np.hypot(np.arctan2(np.floor_divide(x[0], x[0]), np.arcsinh(x[0])), np.expm1(np.arcsinh(x[0]))), np.fmin(224.467, np.cosh(np.expm1(np.arcsinh(np.multiply(x[0], x[1])))))), np.fmin(224.467, np.cosh(np.expm1(np.arcsinh(np.multiply(x[0], x[1])))))), 15.6007)), np.hypot(np.hypot(np.add(np.exp2(np.spacing(x[1])), np.logaddexp2(np.hypot(np.hypot(np.maximum(np.cbrt(np.heaviside(-460.013, x[1])), np.expm1(np.arcsinh(np.multiply(x[0], x[1])))), np.exp2(np.arctan(np.floor_divide(np.fmod(x[1], x[1]), x[1])))), np.fmin(224.467, np.cosh(np.expm1(np.arcsinh(np.multiply(x[0], x[1])))))), np.fmin(x[1], -342.968))), np.fmin(np.multiply(np.fmax(np.hypot(np.arctan2(np.floor_divide(x[0], x[0]), np.arcsinh(x[0])), np.expm1(np.arcsinh(x[0]))), np.fmin(224.467, np.cosh(np.expm1(np.arcsinh(np.multiply(x[0], x[1])))))), np.fmin(224.467, np.cosh(np.expm1(np.arcsinh(np.multiply(x[0], x[1])))))), 15.6007)), np.fmin(np.multiply(np.fmax(-295.787, np.fmin(224.467, np.cosh(np.expm1(np.arcsinh(np.multiply(x[0], x[1])))))), np.fmin(224.467, np.cosh(np.expm1(np.arcsinh(np.multiply(x[0], x[1])))))), 15.6007)))
+
+
+def f8(x: np.ndarray) -> np.ndarray:
+    raise Exception("not implemented")
